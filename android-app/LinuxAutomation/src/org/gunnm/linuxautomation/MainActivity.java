612d2d37b92bcd4240c653bf3f89a360730eca1c
Julien Delange <julien@gunnm.org>
2014-03-20 16:33:25 -0400

    update layout, working now

--- android-app/LinuxAutomation/src/org/gunnm/linuxautomation/MainActivity.java
+++ android-app/LinuxAutomation/src/org/gunnm/linuxautomation/MainActivity.java
@@ -1,8 +1,6 @@
 package org.gunnm.linuxautomation;
 
 import android.app.Activity;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.content.Context;
@@ -10,7 +8,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.LayoutInflater;
-import android.view.Menu;
+import android.view.Menu; 
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,40 +16,15 @@ import android.support.v4.widget.DrawerLayout;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
-public class MainActivity extends Activity
-        implements NavigationDrawerFragment.NavigationDrawerCallbacks {
+public class MainActivity extends Activity {
 
-    /**
-     * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
-     */
-    private NavigationDrawerFragment mNavigationDrawerFragment;
-
-    /**
-     * Used to store the last screen title. For use in {@link #restoreActionBar()}.
-     */
-    private CharSequence mTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        mNavigationDrawerFragment = (NavigationDrawerFragment)
-//                getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
-        mTitle = getTitle();
-
-        // Set up the drawer.
-//        mNavigationDrawerFragment.setUp(
-//                R.id.navigation_drawer,
-//                (DrawerLayout) findViewById(R.id.drawer_layout));
     }
 
-	@Override
-	public void onNavigationDrawerItemSelected(int position) {
-		// TODO Auto-generated method stub
-		
-	}
-
-  
 
 }
