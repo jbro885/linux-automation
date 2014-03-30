ba617d1a8703d956d2999c4fc4819c17344141cd
Julien <julien@gunnm.org>
2014-03-30 09:46:34 -0400

    update interface

--- android-app/LinuxAutomation/src/org/gunnm/linuxautomation/MainActivity.java
+++ android-app/LinuxAutomation/src/org/gunnm/linuxautomation/MainActivity.java
@@ -5,8 +5,11 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu; 
@@ -38,12 +41,28 @@ public class MainActivity extends Activity {
     	// This method is called once the menu is selected
     	@Override
     	public boolean onOptionsItemSelected(MenuItem item) {
-    	  switch (item.getItemId()) {
+    	  switch (item.getItemId())
+    	  {
     	  	case R.id.settings:
-    	  		// Launch settings activity
+    	  	{
     	  		Intent i = new Intent(this, org.gunnm.linuxautomation.SettingsActivity.class);
     	  		startActivity(i);
     	  		return true;
+    	  	}
+    	  	case R.id.refresh:
+    	  	{
+    	  		String serverString;
+    	  		String portString;
+    	  		int port;
+    	  		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+    	  
+    	  		serverString = prefs.getString ("server_addr", "none");
+    	  		portString   = prefs.getString ("server_port", "1234");
+    	  		Log.d("bla", "trying to connect on " + serverString + ":" + portString);
+    	  		new RequestTask().execute("http://stackoverflow.com");
+    	  		
+    	  		return true;
+    	  	}
     	  }
     	  return true;
     	} 
---
+++ android-app/LinuxAutomation/src/org/gunnm/linuxautomation/RequestTask.java
@@ -0,0 +1,52 @@
+package org.gunnm.linuxautomation;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+
+class RequestTask extends AsyncTask<String, String, String>{
+
+    @Override
+    protected String doInBackground(String... uri) {
+        HttpClient httpclient = new DefaultHttpClient();
+        HttpResponse response;
+        String responseString = null;
+        try {
+            response = httpclient.execute(new HttpGet(uri[0]));
+            StatusLine statusLine = response.getStatusLine();
+            if(statusLine.getStatusCode() == HttpStatus.SC_OK){
+                ByteArrayOutputStream out = new ByteArrayOutputStream();
+                response.getEntity().writeTo(out);
+                out.close();
+                responseString = out.toString();
+            } else{
+                //Closes the connection.
+                response.getEntity().getContent().close();
+                throw new IOException(statusLine.getReasonPhrase());
+            }
+        } catch (ClientProtocolException e) {
+        	Log.d("RequestTask", "io exception when trying to connect");
+        } catch (IOException e) {
+        	Log.d("RequestTask", "io exception when trying to connect");
+        }
+       // Log.d("RequestTask", "return" + responseString);
+        return responseString;
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        super.onPostExecute(result);
+        Log.d("RequestTask", "do something on post execute");
+    }
+}
