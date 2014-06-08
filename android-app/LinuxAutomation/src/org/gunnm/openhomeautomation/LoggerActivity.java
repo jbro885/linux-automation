646429d15c265d8df8014d7502d574386e0d5145
Julien <julien@gunnm.org>
2014-06-08 19:20:43 -0400

    Logger starts to work

--- android-app/LinuxAutomation/src/org/gunnm/openhomeautomation/LoggerActivity.java
+++ android-app/LinuxAutomation/src/org/gunnm/openhomeautomation/LoggerActivity.java
@@ -1,9 +1,17 @@
 package org.gunnm.openhomeautomation;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.List;
+
 //import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -11,13 +19,38 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
 public class LoggerActivity extends Activity {
-	public static LoggerActivity instance;
+	
+	List<Map<String, String>> planetsList = new ArrayList<Map<String,String>>();
+		 
+		 
+		   private void initList() {
+		    // We populate the planets
+		 
+		    planetsList.add(createPlanet("planet", "Mercury"));
+		    planetsList.add(createPlanet("planet", "Venus"));
+		    planetsList.add(createPlanet("planet", "Mars"));
+		    planetsList.add(createPlanet("planet", "Jupiter"));
+		    planetsList.add(createPlanet("planet", "Saturn"));
+		    planetsList.add(createPlanet("planet", "Uranus"));
+		    planetsList.add(createPlanet("planet", "Neptune"));
+		 
+		}
+		   
+		   private HashMap<String, String> createPlanet(String key, String name) {
+			   	    HashMap<String, String> planet = new HashMap<String, String>();
+			   	    planet.put(key, name);
+			   	 
+			   	    return planet;
+			   	}
+		 
 
-
+	
 	public void onStart()
 	{
 		super.onStart();
@@ -32,11 +65,18 @@ public class LoggerActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
-		instance = this;
-
+//		Log.d("Logger", "creating the logger");
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_logger);
 
+//		Log.d("Logger", "logger created");
+		
+		ListView myListView = (ListView) findViewById(R.id.allevents);
+	
+		SimpleAdapter simpleAdpt = new SimpleAdapter(this, planetsList, android.R.layout.simple_list_item_1, new String[] {"planet"}, new int[] {android.R.id.text1});
+		myListView.setAdapter(simpleAdpt);
+		initList();
+		
 	}
 
 	public boolean onCreateOptionsMenu(Menu menu) {
